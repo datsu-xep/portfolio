e009b9685abcd1baa3b1e9d95cc9233c068201ef
Datunor Mama <148950469+datsu-xep@users.noreply.github.com>
2023-10-25 18:37:18 +0800

    Add files via upload

---
+++ AMindex.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Me</title>
+    <style>
+        body {
+			
+	       	margin: 0;
+			padding: 0;
+			font-family: 'Roboto', sans-serif;
+			background-image: url("https://images3.alphacoders.com/132/1322308.jpeg");
+			background-repeat: no-repeat;
+			background-size: cover;
+			background-attachment: fixed;
+        }
+		
+		.container {
+			width: 750px;
+			margin-bottom: 10px;
+			font-size: 19px;
+			font-family: Georgia, serif;
+			color: white;
+            text-align: left;
+			line-height: 1.5;
+			padding: 15px;
+			background: transparent;
+			border: 2px solid rgba(225, 225, 225, .2);
+			backdrop-filter: blur(20px);
+            box-shadow: 0 0 10px rgba(0, 0, 0, .2);
+			border-radius: 15px;
+		}
+		
+		img {
+			display: block;
+			margin-top: 20px;
+			margin-left: auto;
+			margin-right: auto;
+			width: 26%;
+			border-radius: 100px;
+			border: solid white 2px;
+		}
+		
+		h3 {
+			color: #0C090A;
+			text-shadow: 3px 3px 5px #000000;
+			font-family: Verdana, sans-serif;
+			margin: 13px;
+		}
+		
+		table, th, td {
+			font-family: Courier New, monospace;
+			font-size: 18px;
+			text-align: center;
+			padding: 10px;
+			margin-left: auto;
+			margin-right: auto;
+			border: 2px solid rgba(225, 225, 225, .2);
+			border-radius: 5px;
+			border-spacing: 8px;
+		}
+		
+		th {
+			color: #000000;
+			font-size: 20px;
+		}
+		
+		.pgraph {
+			text-align: justify;
+			line-height: 1.50;
+			font-size: 18px;
+			font-family: Arial, sans-serif;
+			margin: 30px;
+		}
+		
+		.back {
+			margin-top: 15px;
+			margin-bottom: 0;
+			background-color: #2E8B57; 
+			border-color: #2E8B57;
+			border-radius: 40px;
+			color: black;
+			padding: 12px 28px;
+			font-size: 16px;
+		}
+		
+		.back:hover {
+			background: #00000;
+			color: black;
+			cursor: pointer;
+			box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+		}
+
+    </style>
+</head>
+	<div id="aboutMe">
+		<div class="container">
+		<center><h3><br> Back-End </h3></center>
+			<table>
+				<tr>
+					<th>Name</th>
+					<td>Mama Datunor M.</td>
+				</tr>
+				<tr>
+					<th>Age</th>
+					<td>20</td>
+				</tr>
+				<tr>
+					<th>Youtube Chnnel</th>
+					<td>Datsusenpai</td>
+				</tr>
+				<tr>
+					<th>Address</th>
+					<td>Area 5A Citio Cabuyao, Sauyo Novaliches Q.C</td>
+				</tr>
+				<tr>
+					<th>Hobbies</th>
+					<td>Study Web development, Cat lover, Gamer,</td>
+				</tr>
+				<tr>
+					<th>Strength</th>
+					<td>helping to my groupmates.</td>
+				</tr>
+				<tr>
+					<th>Gmail</th>
+					<td>dmmama2759qc@student.fatima.ph</td>
+				</tr>
+			</table>
+	
+				<div class="pgraph">
+					<p>Back-end developer are the experts who build and maintain the mechanisms that process data and perform actions on websites.
+					U nlike front-end developers, who control everything you can see on a website, back-end developers are involved in data storage, security, and other server-side functions that you cannot see..</p>
+					
+					<p>As a back-End developer responsibilities include creating, maintaining, testing, and debugging the entire back end of an application or system.
+					This includes the core application logic, databases, data and application integration, API, and other processes taking place behind the scenes.
+ .</p>
+					
+				<div class="index">
+					<a href="AboutMe.php"><button class="back">Back to Index</button></a>
+				</div>
+			</div>
+		</div>
+    </div>
+<body>
+    
+</body>
+</html>
---
+++ PORTFOLIO - About me/AboutMe.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+	<style>
+		body {
+			
+			margin: 0;
+		 padding: 0;
+		 font-family: 'Roboto', sans-serif;
+		 background-image: url("https://i.pinimg.com/originals/c9/dd/42/c9dd42f0c4b4e60f8722a9550f13ac30.jpg");
+		 background-repeat: no-repeat;
+		 background-size: cover;
+		 background-attachment: fixed;
+		 text-align: center;
+		} 
+		
+		h2 {
+			font-family: Papyrus, fantasy;
+			font-size: 45px;
+		}
+		
+		p {
+			font-family: Arial, sans-serif
+			font-size: 20px;
+		}
+		.next {
+			margin-top: 5%;
+			margin-bottom: 10px;
+			background-color: #2E8B57; 
+			border-color: #2E8B57;
+			border-radius: 40px;
+			color: black;
+			padding: 10px 26px;
+			font-size: 16px;
+		}
+		
+ </style>
+<body>
+    <div class="container">
+        <h2>WELCOME TO MY ABOUT ME</h2>
+			<p>Hello!, Welcome to my Information.
+			Please Click The Proceed The Continue My AboutMe Page Thankyou!</p>
+				<a href="AMindex.php"><button class="next">Proceed</button></a>
+    </div>
+</body>
+</html>
